d34b7f3c1fc3102e2a1534795b2d79d3920bd4b0
helloMJW <majianwei168@outlook.com>
2018-01-05 11:39:15 +0800

    asgardcms-部署脚本

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # dockerPhpDemo
 相关PHP开源项目的环境, 一键部署开源系统
+
+## asgardcms
+
+[GITHUB-AsgardCms](https://github.com/AsgardCms/Platform)
+
+## 使用
+
+*HOST文件-d.asgardcms.com 指向到docker中*
+
+后台登录地址: d.asgardcms.com/en/backend
+
+- 用户名: admin@gmail.com
+- 密码:   admin123
+
--- wwwroot/install.sh
+++ wwwroot/install.sh
@@ -1,4 +1,21 @@
 #! /bin/sh
-#apk add expect
-#composer create-project asgardcms/platform asgardcms
-./expect_a.exp
+#判断是否已经部署
+
+apk add expect
+apk add mysql-client
+#创建数据库
+HOSTNAME='mysql5.6'
+PORT='3306'
+USERNAME='root'
+PASS='root'
+
+DBNAME='asgardcms'
+
+MYSQL_CMD="mysql -h ${HOSTNAME} -P ${PORT} -u ${USERNAME} -p${PASS}"
+echo ${MYSQL_CMD}
+echo "create database ${DBNAME}"
+create_db_sql="create database IF NOT EXISTS ${DBNAME} DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
+echo ${create_db_sql} | ${MYSQL_CMD}
+
+composer create-project asgardcms/platform asgardcms
+./asgardcms.exp
