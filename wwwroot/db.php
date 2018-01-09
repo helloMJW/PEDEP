9ff3b46ced58dd6d777b383e176f34b360ef1614
helloMJW <majianwei168@outlook.com>
2018-01-09 09:22:49 +0800

    安装配置脚本完善

--- wwwroot/db.php
+++ wwwroot/db.php
@@ -1,4 +1,3 @@
 <?php
 $dbh = new PDO('mysql:host=mysql5.6', 'root', 'root');
-$count = $dbh->exec("CREATE DATABASE `october` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;");
-var_dump($count);
+$num = $dbh->exec("CREATE DATABASE `october` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;");
--- wwwroot/install.sh
+++ wwwroot/install.sh
@@ -5,9 +5,9 @@ then
     rm -r $project_dir
 fi
 mkdir $project_dir
-chmod -R 777 $project_dir 
 php db.php
 cd $project_dir
 php -r "eval('?>'.file_get_contents('https://octobercms.com/api/installer'));"
 cp -f ../OctoberInstall.php ./modules/system/console/
 php artisan october:install
+chmod -R 777 ../$project_dir 
